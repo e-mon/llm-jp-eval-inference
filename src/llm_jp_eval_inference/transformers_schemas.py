637e85edd0d0fc32386df8575814f7fabd0e79b8
e-mon <emon18@icloud.com>
2025-01-14 14:56:44 +0900

    implement pydantic base config

--- src/llm_jp_eval_inference/transformers_schemas.py
+++ src/llm_jp_eval_inference/transformers_schemas.py
@@ -1,7 +1,8 @@
-from typing import Literal
-from pydantic import BaseModel
+from typing import Literal, Any
+
+from pydantic import BaseModel, ConfigDict, Field, field_serializer, field_validator
 from transformers import GenerationConfig
-from llm_jp_eval_inference.schemas import BaseInferenceConfig, WandBConfig
+from llm_jp_eval_inference.schemas import BaseInferenceConfig
 
 class ModelConfig(BaseModel):
     pretrained_model_name_or_path: str
@@ -17,23 +18,23 @@ class TokenizerConfig(BaseModel):
     use_fast: bool = True
     padding_side: Literal["left", "right"] = "left"
     model_max_length: int = 2048
-    add_special_tokens: bool = False
 
-# these values will be used only for logging and wandb (no effect for running environment)
-class MetaInfo(BaseModel):
-  quantization: str | None = None
-  run_name_lib_type: str | None = None
-  run_name_gpu_type: str | None = None
-  run_name_suffix: str | None = None
-  model_conversion_time: float = 0.0
-
-class InferenceConfig(BaseModel):
-    inference_config: BaseInferenceConfig
-    wandb: WandBConfig
+class InferenceConfig(BaseInferenceConfig):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
 
     # transformers specific configurations
-    run: MetaInfo
-    model: ModelConfig
-    tokenizer: TokenizerConfig
-    generation_config: GenerationConfig
+    model: ModelConfig = Field(default_factory=ModelConfig)
+    tokenizer: TokenizerConfig = Field(default_factory=TokenizerConfig)
+    generation_config: GenerationConfig = Field(default_factory=GenerationConfig)
+    pipeline_kwargs: dict = Field(default_factory=dict)
 
+    @field_validator("generation_config", mode='before')
+    @classmethod
+    def validate_generation_config(cls, value: Any) -> GenerationConfig:
+        if isinstance(value, dict):
+            return GenerationConfig(**value)
+        return value
+
+    @field_serializer("generation_config")
+    def serialize_generation_config(self, config: GenerationConfig) -> dict:
+        return config.to_dict()
